ed3e24042858911fb28dab4643058a62e6a28f6b
Hagar Shilo <hgr@Hagars-MacBook-Air.local>
2016-09-25 21:27:45 +0300

    Changes to contact.php

--- includes/contact.php
+++ includes/contact.php
@@ -2,7 +2,7 @@
 
 if ($_POST['submit']) {
   if (!$_POST['name']){
-    $error="<br/>שכחת לכתוב שמך. ";
+    $error="<br/>שכחת לכתוב את השם שלך. ";
   }
   if (!$_POST['email']){
     $error.="<br/>חסרה כתובת המייל. ";
@@ -10,7 +10,7 @@ if ($_POST['submit']) {
   if (!$_POST['msg']){
     $error.="<br/>לא כתבת את ההודעה. ";
   }
-  if (!$_POST['checkbox']){
+  if (!$_POST['check']){
     $error.="<br/>נא לציין אם את/ה דג. ";
   }
   if ($error) {
@@ -30,7 +30,7 @@ if ($_POST['submit']) {
 <div class="modal fade" id="contact" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
-      <form class="form-horizontal" method="post" role="form">
+      <form class="form-horizontal" method="post" action="index2.php" role="form">
         <div class="modal-header">
           <h4>יצירת קשר</h4>
           <?php
@@ -39,30 +39,27 @@ if ($_POST['submit']) {
         <div class="modal-body">
 
           <div class="form-group">
-            <label for="contact-name" class="col-lg-2 control-label"></label>
-              <div class="col-lg-10">
-                <input type="text" name="name" class="form-control"
-                  id="contact-name" placeholder="שם מלא"
-                  value="<?php echo $_POST['name'];?>">
-              </div>
+            <div class="col-lg-12">
+              <input type="text" name="name" class="form-control"
+                id="contact-name" placeholder="שם מלא"
+                value="<?php echo $_POST['name'];?>">
+            </div>
           </div>
 
           <div class="form-group">
-            <label for="contact-email" class="col-lg-2 control-label"></label>
-              <div class="col-lg-10">
-                <input type="email" name="email" class="form-control"
-                  id="contact-email" placeholder="you@example.com"
-                  value="<?php echo $_POST['email'];?>">
-              </div>
+            <div class="col-lg-12">
+              <input type="email" name="email" class="form-control"
+                id="contact-email" placeholder="you@example.com"
+                value="<?php echo $_POST['email'];?>">
+            </div>
           </div>
 
           <div class="form-group">
-            <label for="contact-msg" class="col-lg-2 control-label"></label>
-              <div class="col-lg-10">
-                <textarea class="form-control" rows="8" placeholder="הודעה...">
-                  <?php echo $_POST['msg'];?>
-                </textarea>
-              </div>
+            <div class="col-lg-12">
+              <textarea class="form-control" rows="8" placeholder="הודעה..." name="msg"><?php
+                echo $_POST['msg'];
+              ?></textarea>
+            </div>
           </div>
 
           <div class="checkbox">
@@ -73,7 +70,7 @@ if ($_POST['submit']) {
         </div>
 
         <div class="modal-footer">
-          <button class="btn btn-primary btn-sm" type="submit" value="Submit">שליחה</button>
+          <button class="btn btn-primary btn-sm" type="submit" value="Submit" name="submit">שליחה</button>
           <a class="btn btn-default btn-sm" data-dismiss="modal">ביטול</a>
         </div>
 
