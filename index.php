eeab7865e9e5f85f42ccb8eed2ef739f5bb2cfcc
Hagar Shilo <strayblues@gmail.com>
2017-01-06 17:33:39 +0200

    Modify bg img opacity and remove unused file

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="he" dir="rtl">
   <head>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-88935594-3', 'auto');
+    ga('send', 'pageview');
+  </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
@@ -54,7 +63,7 @@
         <a href="procedure.html">
           <div>
             <h2>
-              מידע על התהליך
+              איך עושים את זה?
             </h2>
             <p>
               <?php
