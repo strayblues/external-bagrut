39b9a07b2d1e8332e1afcea5a9e4e641404a5086
Hagar Shilo <strayblues@gmail.com>
2017-02-09 15:39:01 +0200

    Change encoding to iso-8859-8 for better Hebrew support

--- parser.php
+++ parser.php
@@ -1,6 +1,6 @@
 <?php
 if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
-	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
+	$n = $_POST['n']; // use preg_replace() to filter the data
 	$e = $_POST['e'];
 	$m = nl2br($_POST['m']); // Preserve newlines entered by the user
 	$to = "strayblues@gmail.com";
@@ -10,12 +10,12 @@ if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
 	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
 	$headers = "From: $from\n";
 	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+	$headers .= "Content-type: text/html; charset=iso-8859-8\n"; 
   // Run the mail function
 	if( mail($to, $subject, $message, $headers) ){
 		echo "success";
 	} else {
-		echo "The server failed to send the message. Please try again later.";
+		echo "ההודעה לא נשלחה. נסי שנית מאוחרכך.";
 	}
 }
 ?>
